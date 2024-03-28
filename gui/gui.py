9e5fb442bba3ec8cdb500f828bd7b4a05a63e0b3
Ali Kesserwani <akesser@uab.edu>
2024-03-28 12:53:37 -0500

    added functions to the buttons

--- gui/gui.py
+++ gui/gui.py
@@ -5,10 +5,67 @@ from helpers import *
 # PROJECT DOCS
 # https://docs.google.com/document/d/1nPIF0tEGJduzCj4uC4_EYdg9k11v1ASdr1NoHzhgJb8/edit
 
+#Functions for buttons
 def testFunction():
     #TODO make drone emergency land.
     print("test")
 
+#take off function
+def takeOff():
+    print("Take off")
+
+#function to land
+def land():
+    print("Land")
+
+#function to increase alt/go up
+def increaseAlt():
+    print("Increase Alt")
+
+#function to decrease alt/go down
+def decreaseAlt():
+    print("Decrease Alt")
+
+#function to turn right
+def turnRight():
+    print("Turn Right")
+    
+#function to yaw right
+def yawRight():
+    print("Yaw Right")
+
+#function to trigger movements
+    
+#Square Shape
+def movement1():
+    print("Movement 1")
+    
+def movement2():
+    print("Movement 2")
+
+def movement3():
+    print("Movement 3")
+
+def movement4():
+    print("Movement 4")
+
+#function to turn left
+def turnLeft():
+    print("Turn Left")
+    
+#function to yaw left
+def yawLeft():
+    print("Yaw Left")
+
+#emergency button
+#KILLS ALL ENGINES
+def emergency():
+    print("Killing all engines")
+
+
+
+
+#main function for the UI
 def main():
     # set theme to our custom theme
     # see this for more information:    https://customtkinter.tomschimansky.com/documentation/color
@@ -36,7 +93,7 @@ def main():
     speedLabel = ctk.CTkLabel(innerWestFrame, text="Speed (cm/s)")
     speedValueLabel = ctk.CTkLabel(innerWestFrame, text="100", text_color="gray")
     
-    emergencyButton = ctk.CTkButton(outerWestFrame, text="Emergency", width=100, height=30, command=testFunction)
+    emergencyButton = ctk.CTkButton(outerWestFrame, text="Emergency", width=100, height=30, command=emergency())
     
     leftColPaddingX = 25
     PaddingY = 20
@@ -57,8 +114,8 @@ def main():
     # Create the turn left frame
     innerLeftFrame, outerLeftFrame = createNiceFrame(win, x=200, y=200)
 
-    turnLeftButton = ctk.CTkButton(innerLeftFrame, text="turn left", command=testFunction)
-    yawLeftButton = ctk.CTkButton(innerLeftFrame, text="yaw left", command=testFunction)
+    turnLeftButton = ctk.CTkButton(innerLeftFrame, text="turn left", command=turnLeft())
+    yawLeftButton = ctk.CTkButton(innerLeftFrame, text="yaw left", command=yawLeft())
 
     turnLeftButton.pack(ipadx=25, ipady=10, pady=7)
     yawLeftButton.pack(ipadx=25, ipady=10, pady=7)
@@ -67,8 +124,8 @@ def main():
     # Create the turn right frame
     innerRightFrame, outerRightFrame = createNiceFrame(win, x=200, y=200)
 
-    turnRightButton = ctk.CTkButton(innerRightFrame, text="turn right", command=testFunction)
-    yawRightButton = ctk.CTkButton(innerRightFrame, text="yaw right", command=testFunction)
+    turnRightButton = ctk.CTkButton(innerRightFrame, text="turn right", command=turnRight())
+    yawRightButton = ctk.CTkButton(innerRightFrame, text="yaw right", command=yawRight())
 
     turnRightButton.pack(ipadx=25, ipady=10, pady=7)
     yawRightButton.pack(ipadx=25, ipady=10, pady=7)
@@ -79,7 +136,7 @@ def main():
     innerSouthFrame, outerSouthFrame = createNiceFrame(win, x=500, y=200)
     preprogrammedButtons = []
     for i in range(4):
-        preprogrammedButtons.append(ctk.CTkButton(innerSouthFrame, width=200, height=40, text="Movement 1", command=testFunction))
+        preprogrammedButtons.append(ctk.CTkButton(innerSouthFrame, width=200, height=40, text="Movement " + str(i + 1), command=movement1))
         # WHEN THE FUNCTION OF THIS BUTTON IS ASSIGNED, BE SURE TO ASSIGN CORRECT COMMANDS AS WELL
     
     preprogrammedButtons[0].grid(row=0, column=0, padx=10, pady=10)
@@ -92,10 +149,10 @@ def main():
     # Create the controls frame on the east
     innerEastFrame, outerEastFrame = createNiceFrame(win, x=200, y=400)
 
-    takeOffButton = ctk.CTkButton(innerEastFrame, text="take off", command=testFunction)
+    takeOffButton = ctk.CTkButton(innerEastFrame, text="take off", command=takeOff)
     landButton = ctk.CTkButton(innerEastFrame, text="land", command=testFunction)
-    increaseAltitudeButton = ctk.CTkButton(innerEastFrame, text="+ Altitude", command=testFunction)
-    decreaseAltitudeButton = ctk.CTkButton(innerEastFrame, text="- Altitude", command=testFunction)
+    increaseAltitudeButton = ctk.CTkButton(innerEastFrame, text="+ Altitude", command=increaseAlt())
+    decreaseAltitudeButton = ctk.CTkButton(innerEastFrame, text="- Altitude", command=decreaseAlt())
 
     takeOffButton.pack(ipadx=25, ipady=10, pady=7)
     landButton.pack(ipadx=25, ipady=10, pady=7)
